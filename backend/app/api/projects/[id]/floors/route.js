cc384f82feafbc609bcc7621cc71f6ebf97faf1c
lakshya-02 <lakshya.singh2706@gmail.com>
2026-05-15 21:07:31 +0530

    fix: resolve dynamic floors route params

--- backend/app/api/projects/[id]/floors/route.js
+++ backend/app/api/projects/[id]/floors/route.js
@@ -1,5 +1,6 @@
 import { getFloors } from "../../../../../lib/data";
 
 export async function GET(_req, { params }) {
-  return Response.json(getFloors(params.id));
+  const { id } = await params;
+  return Response.json(getFloors(id));
 }
